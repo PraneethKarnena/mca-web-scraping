25496695e751e1f385b53ed22e131d1c82cb5669
Praneeth Karnena <praneeth.codes@gmail.com>
2021-07-17 21:26:49 +0530

    Update the API URL

--- scrape.py
+++ scrape.py
@@ -18,59 +18,61 @@ def parse_cin():
 
 def download_webpage(cin):
     try:
-        URL = 'http://www.mca.gov.in/mcafoportal/companyLLPMasterData.do'
-        post_body = {'companyID': cin}
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.post(URL, headers=headers, data=post_body)
+        URL = 'https://www.quickcompany.in/company/autocomplete?term=' + cin
+        response = requests.get(URL).json()
+        
         if response.status_code != 200:
             raise Exception('Cannot fetch webpage!')
-        webpage = response.content
-        extract_content(webpage)
+            
+        print(json.dumps(response))
+            
+#         webpage = response.content
+#         extract_content(webpage)
 
     except Exception as e:
         print(f'Error: {str(e)}')
 
 
-def extract_content(webpage):
-    try:
-        soup = BeautifulSoup(webpage, 'html.parser')
+# def extract_content(webpage):
+#     try:
+#         soup = BeautifulSoup(webpage, 'html.parser')
 
-        company_table = soup.find('table', attrs={'id': 'resultTab1'}).find_all('td')
-        director_table = soup.find('table', attrs={'id': 'resultTab6'}).find_all('td')
+#         company_table = soup.find('table', attrs={'id': 'resultTab1'}).find_all('td')
+#         director_table = soup.find('table', attrs={'id': 'resultTab6'}).find_all('td')
 
-        for i in range(len(company_table)):
-            company_table[i] = remove_special_chars(company_table[i].text)
-        for i in range(len(director_table)):
-            director_table[i] = remove_special_chars(director_table[i].text)
+#         for i in range(len(company_table)):
+#             company_table[i] = remove_special_chars(company_table[i].text)
+#         for i in range(len(director_table)):
+#             director_table[i] = remove_special_chars(director_table[i].text)
 
-        company_data = {}
-        for i in range(0, len(company_table), 2):
-            company_data[company_table[i]] = company_table[i+1]
+#         company_data = {}
+#         for i in range(0, len(company_table), 2):
+#             company_data[company_table[i]] = company_table[i+1]
 
-        NO_OF_DIR_COLS = 5
-        director_data = []
-        director_data_cols = ['DIN/PAN', 'Name', 'Begin date', 'End date', 'Date of Modification', 'Status']
-        for i in range(0, len(director_table), NO_OF_DIR_COLS):
-            director_data.append(director_table[i:i+NO_OF_DIR_COLS])
+#         NO_OF_DIR_COLS = 5
+#         director_data = []
+#         director_data_cols = ['DIN/PAN', 'Name', 'Begin date', 'End date', 'Date of Modification', 'Status']
+#         for i in range(0, len(director_table), NO_OF_DIR_COLS):
+#             director_data.append(director_table[i:i+NO_OF_DIR_COLS])
 
-        for i in range(len(director_data)):
-            director_data[i] = dict(zip(director_data_cols, director_data[i]))
+#         for i in range(len(director_data)):
+#             director_data[i] = dict(zip(director_data_cols, director_data[i]))
 
-        print_data(company_data, director_data)
+#         print_data(company_data, director_data)
 
-    except Exception as e:
-        print(f'Error: {str(e)}')
-
-
-def print_data(company_data, director_data):
-    print('\n---------- COMPANY DATA ----------\n')
-    print(json.dumps(company_data, sort_keys=True, indent=4))
-    print('\n--------- Directors Data ---------\n')
-    print(json.dumps(director_data, sort_keys=True, indent=4))
+#     except Exception as e:
+#         print(f'Error: {str(e)}')
 
 
-def remove_special_chars(s):
-    return s.rstrip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
+# def print_data(company_data, director_data):
+#     print('\n---------- COMPANY DATA ----------\n')
+#     print(json.dumps(company_data, sort_keys=True, indent=4))
+#     print('\n--------- Directors Data ---------\n')
+#     print(json.dumps(director_data, sort_keys=True, indent=4))
+
+
+# def remove_special_chars(s):
+#     return s.rstrip().replace('\n', '').replace('\t', '').replace('\r', '').strip()
 
 
 if __name__ == '__main__':
